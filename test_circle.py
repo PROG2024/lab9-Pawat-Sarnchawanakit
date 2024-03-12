3b145cf014cae3747d8bbfc55e735d135b6c9e9c
Pawat Sarnchawanakit <pawat.sar@ku.th>
2024-03-12 15:50:21 +0700

    Implemented unittest

--- test_circle.py
+++ test_circle.py
@@ -8,5 +8,33 @@ Write unit tests as described in README.md.
 
 """
 from circle import Circle
+import unittest
+from unittest import TestCase
 
-# TODO write 3 tests as described above
+
+class TestCircle(TestCase):
+
+    def test_add_area(self):
+        circle = Circle(10)
+        circle_2 = Circle(10)
+        new_circle = circle.add_area(circle_2)
+        self.assertAlmostEqual(new_circle.get_radius(),
+                               14.142135623730951,
+                               places=5)
+        self.assertEqual(circle.get_radius(), 10)
+        self.assertEqual(circle_2.get_radius(), 10)
+
+    def test_add_area_edge(self):
+        circle_2 = Circle(10)
+        circle_3 = Circle(0)
+        new_circle = circle_2.add_area(circle_3)
+        self.assertEqual(new_circle.get_radius(), 10)
+        new_circle = circle_3.add_area(circle_2)
+        self.assertEqual(new_circle.get_radius(), 10)
+
+    def test_circle_constructor(self):
+        self.assertRaises(Exception, Circle, -1)
+
+
+if __name__ == "__main__":
+    unittest.main()
